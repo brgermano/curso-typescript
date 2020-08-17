c1d144791cd34bb25764fb82530ae12336c4db75
Bruno Germano <brunoalvesgermano@gmail.com>
2020-08-16 21:41:42 -0300

    Simple examples of numbers and boolean type

--- tipos/tipo.js
+++ tipos/tipo.js
@@ -3,3 +3,12 @@
 var nome = 'joao';
 console.log(nome);
 // nome = 28
+// numbers
+var idade = 27;
+// idade = 'ana'
+idade = 27.5;
+console.log(idade);
+//boolean
+var possuiHobbies = false;
+// possuiHobbies = 1
+console.log(possuiHobbies);
--- tipos/tipo.ts
+++ tipos/tipo.ts
@@ -3,3 +3,13 @@ let nome = 'joao'
 console.log(nome)
 // nome = 28
 
+// numbers
+let idade = 27
+// idade = 'ana'
+idade = 27.5
+console.log(idade)
+
+//boolean
+let possuiHobbies = false
+// possuiHobbies = 1
+console.log(possuiHobbies)
