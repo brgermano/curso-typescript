e6a251b00ebb8c418dfdb52ebec6ed3fcc1d7d92
Bruno Germano <brunoalvesgermano@gmail.com>
2020-08-20 08:07:57 -0300

    Example of object type

--- tipos/tipo.js
+++ tipos/tipo.js
@@ -83,3 +83,26 @@ usuario = {
     nome: 'Maria',
 };
 console.log("usuario", usuario);
+// Desafio
+/*
+  Criar um objeto funcionário com:
+    - Array de strings com os nomes dos supervisores
+    - Funções de bater ponto que recebe a hora (numero)
+      e retorna uma string
+        -> Ponto normal (<= 8)
+        -> Fora do horario (> 8)
+*/
+var funcionario = {
+    supervisores: ['Ana', 'Fernando'],
+    baterPonto: function (horario) {
+        if (horario <= 8) {
+            return 'Ponto normal';
+        }
+        else {
+            return 'Fora do horário';
+        }
+    }
+};
+console.log(funcionario.supervisores);
+console.log(funcionario.baterPonto(8));
+console.log(funcionario.baterPonto(9));
--- tipos/tipo.ts
+++ tipos/tipo.ts
@@ -100,3 +100,32 @@ usuario = {
   nome: 'Maria',
 }
 console.log("usuario", usuario)
+
+// Desafio
+
+/* 
+  Criar um objeto funcionário com:
+    - Array de strings com os nomes dos supervisores
+    - Funções de bater ponto que recebe a hora (numero)
+      e retorna uma string 
+        -> Ponto normal (<= 8)
+        -> Fora do horario (> 8)
+*/
+
+let funcionario: {
+  supervisores: string[],
+  baterPonto: (horas: number) => string
+} = {
+  supervisores: ['Ana', 'Fernando'],
+  baterPonto(horario: number): string {
+    if(horario <= 8) {
+      return 'Ponto normal'
+    } else {
+      return 'Fora do horário'
+    }
+  }
+}
+
+console.log(funcionario.supervisores)
+console.log(funcionario.baterPonto(8))
+console.log(funcionario.baterPonto(9))
